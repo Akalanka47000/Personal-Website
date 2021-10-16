46e41f7f27ab3790de668ee1bf09a84304854dff
Akalanka Perera <akalankaperera128@gmail.com>
2021-10-16 23:56:07 +0530

    Update audioSpectrum.tsx

--- components/common/audioSpectrum.tsx
+++ components/common/audioSpectrum.tsx
@@ -1,14 +1,24 @@
 import { useEffect } from 'react'
 import AudioSpectrum from 'react-audio-spectrum'
-import $ from "jquery"
+import $ from 'jquery'
 
 const Spectrum = (): JSX.Element => {
   useEffect(() => {
     setTimeout(() => {
-      $('#dummyBtn').click()
+      eventFire(document.getElementById('dummyBtn'), 'click')
     }, 4000)
   }, [])
-  
+
+  function eventFire(el, etype) {
+    if (el.fireEvent) {
+      el.fireEvent('on' + etype)
+    } else {
+      var evObj = document.createEvent('Events')
+      evObj.initEvent(etype, true, false)
+      el.dispatchEvent(evObj)
+    }
+  }
+
   const playAudio = () => {
     const audio = document.getElementById('audio-element') as HTMLAudioElement
     audio.muted = true
@@ -30,20 +40,19 @@ const Spectrum = (): JSX.Element => {
 
   return (
     <div>
-      
       <div
         className="absolute w-screen bottom-0 left-0 flex justify-center opacity-80"
         id="spectrum-container"
       >
         <button id="dummyBtn" onClick={playAudio}>
-        <audio
-          id="audio-element"
-          src="/portfolio/audio/music.mp3"
-          autoPlay={false}
-          muted
-          loop
-        ></audio>
-      </button>
+          <audio
+            id="audio-element"
+            src="/portfolio/audio/music.mp3"
+            autoPlay={false}
+            muted
+            loop
+          ></audio>
+        </button>
         <AudioSpectrum
           id="audio-canvas"
           height={150}
