18479bd065d46b68c0b6642f6136aabd45c80eda
Akalanka Perera <akalankaperera128@gmail.com>
2021-10-16 19:42:51 +0530

    Update audioSpectrum.tsx

--- components/common/audioSpectrum.tsx
+++ components/common/audioSpectrum.tsx
@@ -2,23 +2,17 @@ import AudioSpectrum from 'react-audio-spectrum'
 
 const Spectrum = (): JSX.Element => {
   if (process.browser) {
-    const audioElement = document.getElementById(
-      'audio-element'
-    ) as HTMLAudioElement
     document.body.addEventListener('mousemove', function () {
+      const audioElement = document.getElementById(
+        'audio-element'
+      ) as HTMLAudioElement
       audioElement.volume = 0.1
       audioElement.play()
     })
   }
   return (
     <div>
-      <audio
-        id="audio-element"
-        src="/portfolio//audio/music.mp3"
-        muted={false}
-        loop
-
-      ></audio>
+      <audio id="audio-element" src="/portfolio//audio/music.mp3" loop></audio>
       <div
         className="absolute w-screen bottom-0 left-0 flex justify-center opacity-80"
         id="spectrum-container"
