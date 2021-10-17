764441080cb027d250edba3def328e008863c2ab
Akalanka Perera <akalankaperera128@gmail.com>
2021-10-17 12:43:07 +0530

    Minor hotfix

--- components/common/audioSpectrum.tsx
+++ components/common/audioSpectrum.tsx
@@ -20,7 +20,7 @@ const Spectrum = (): JSX.Element => {
   return (
     <>
       <div
-        className="w-20 h-20 flex justify-center items-center bg-orange-dark rounded-full cursor-pointer sticky bottom-8 left-full mr-8 mb-8 filter hover:brightness-125 transition ease duration-500"
+        className="w-17 h-17 lg:w-20 lg:h-20 flex justify-center items-center bg-orange-dark rounded-full cursor-pointer sticky bottom-8 left-full mr-8 mb-8 filter hover:brightness-125 transition ease duration-500"
         onClick={toggleAudio}
       >
         {playing ? (
@@ -31,29 +31,29 @@ const Spectrum = (): JSX.Element => {
       </div>
       <audio id="audio-element" src="/portfolio/audio/music.mp3" loop></audio>
       <div
-        className="absolute w-screen bottom-0 left-0 flex justify-center opacity-80"
+        className="w-screen absolute bottom-0 l opacity-80"
         id="spectrum-container"
       >
-        <AudioSpectrum
-          id="audio-canvas"
-          height={150}
-          width={
-            process.browser
-              ? document.getElementById('spectrum-container')?.clientWidth
-              : 1920
-          }
-          audioId={'audio-element'}
-          capColor={'#ff4000'}
-          capHeight={6}
-          meterWidth={3}
-          meterCount={1000}
-          meterColor={[
-            { stop: 0, color: '#ffffff' },
-            { stop: 0.5, color: '#000000' },
-            { stop: 1, color: '#ffffff' },
-          ]}
-          gap={8}
-        />
+        {process.browser ? (
+          <AudioSpectrum
+            id="audio-canvas"
+            height={150}
+            width={screen.width}
+            audioId={'audio-element'}
+            capColor={'#ff4000'}
+            capHeight={6}
+            meterWidth={3}
+            meterCount={1000}
+            meterColor={[
+              { stop: 0, color: '#ffffff' },
+              { stop: 0.5, color: '#000000' },
+              { stop: 1, color: '#ffffff' },
+            ]}
+            gap={8}
+          />
+        ) : (
+          <div></div>
+        )}
       </div>
     </>
   )
