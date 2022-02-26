cfcf2fb2732406fde62fd864823af0e0ff3aea17
Akalanka Perera <akalanka@parallax.lk>
2022-02-27 02:48:20 +0530

    Updated skill card

--- components/skills/skillCard.tsx
+++ components/skills/skillCard.tsx
@@ -1,25 +1,40 @@
 interface cardProps {
+  name: string
   logo: string
   width: string
+  link: string
 }
 
-const SkillCard = ({ logo, width }: cardProps): JSX.Element => {
-  const borderStyle =
-    'border-gray-400 transform scale-110 absolute z-0'
+const SkillCard = ({ name, logo, width, link }: cardProps): JSX.Element => {
+  const borderStyle = 'border-gray-400 transform scale-110 absolute z-0'
   return (
-    <div className="w-33vw h-33vw md:w-20vw md:h-20vw xl:w-14vw xl:h-14vw rounded-2xl relative transform hover:scale-105 transition ease-in ">
-      <div className={`${borderStyle} border-t-4 top-0 left-0 h-full clipWidth`}></div>
-      <div className={`${borderStyle} border-b-4 bottom-0 right-0 h-full clipWidth`}></div>
-      <div className={`${borderStyle} border-l-4 bottom-0 w-full clipHeight`}></div>
-      <div className={`${borderStyle} border-r-4 top-0 w-full clipHeight`}></div>
-      <div className="w-full h-full bg-gray-dark border-orange-dark border absolute top-0 left-0 z-10 shadow-xl flex justify-center items-center ">
-        <img
-          src={`/portfolio/skills/${logo}.png`}
-          alt="skill"
-          style={{ width: `${width}` }}
-        />
+    <a href={link} target="_blank">
+      <div className="w-33vw h-33vw md:w-20vw md:h-20vw xl:w-14vw xl:h-14vw rounded-2xl cursor-pointer group relative transform hover:scale-105 transition ease-in">
+        <div
+          className={`${borderStyle} border-t-4 top-0 left-0 h-full clipWidth`}
+        ></div>
+        <div
+          className={`${borderStyle} border-b-4 bottom-0 right-0 h-full clipWidth`}
+        ></div>
+        <div
+          className={`${borderStyle} border-l-4 bottom-0 w-full clipHeight`}
+        ></div>
+        <div
+          className={`${borderStyle} border-r-4 top-0 w-full clipHeight`}
+        ></div>
+        <div className="w-full h-full bg-gray-dark border-orange-dark border absolute top-0 left-0 z-10 shadow-xl flex justify-center items-center ">
+          <img
+            src={`/portfolio/skills/${logo}.png`}
+            alt="skill"
+            style={{ width: `${width}` }}
+          />
+        </div>
+        <div className="w-full h-0 group-hover:h-full opacity-0 group-hover:opacity-80 bg-gray-dark absolute bottom-0 left-0 z-10 flex justify-center items-center transition-all ease-out duration-500" />
+        <div className="w-full h-0 group-hover:h-full opacity-0 group-hover:opacity-100 absolute bottom-0 left-0 z-10 flex justify-center items-center transition-all ease-out duration-500">
+          <div className="text-white text-3xl">{name}</div>
+        </div>
       </div>
-    </div>
+    </a>
   )
 }
 
--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -2,54 +2,76 @@ import SkillCard from './skillCard'
 
 const skillSet = [
   {
+    name: 'Flutter',
     image: 'flutter',
     logoWidth: '53%',
+    link: 'https://flutter.dev/'
   },
   {
+    name: 'React',
     image: 'react',
     logoWidth: '65%',
+    link: 'https://reactjs.org/'
   },
   {
+    name: 'Vue',
     image: 'vue',
     logoWidth: '65%',
+    link: 'https://vuejs.org/'
   },
   {
+    name: 'Angular',
     image: 'angular',
     logoWidth: '52%',
+    link: 'https://angular.io/'
   },
   {
+    name: 'Node',
     image: 'node',
     logoWidth: '45%',
+    link: 'https://nodejs.org/en/'
   },
   {
+    name: 'Laravel',
     image: 'laravel',
     logoWidth: '50%',
+    link: 'https://laravel.com/docs/9.x'
   },
   {
+    name: 'Java',
     image: 'java',
     logoWidth: '62%',
+    link: 'https://www.java.com/en/'
   },
   {
+    name: 'MongoDB',
     image: 'mongo',
     logoWidth: '73%',
+    link: 'https://www.mongodb.com/'
   },
   {
+    name: 'MySQL',
     image: 'mysql',
     logoWidth: '73%',
+    link: 'https://www.mysql.com/'
   },
-  ,
   {
+    name: 'Firebase',
     image: 'firebase',
     logoWidth: '58%',
+    link: 'https://firebase.google.com/'
   },
   {
+    name: 'Github Actions',
     image: 'ghactions',
     logoWidth: '58%',
+    link: 'https://github.com/features/actions'
   },
-  ,
   {
+    name: 'After Effects',
     image: 'AE',
     logoWidth: '65%',
+    link: 'https://www.adobe.com/products/aftereffects.html'
   },
 ]
 const Skills = (): JSX.Element => {
@@ -70,8 +92,10 @@ const Skills = (): JSX.Element => {
               key={index}
             >
               <SkillCard
+                name= {skill?.name || ''}
                 logo={skill?.image || ''}
                 width={skill?.logoWidth || ''}
+                link={skill?.link || ''}
               />
             </div>
           )
